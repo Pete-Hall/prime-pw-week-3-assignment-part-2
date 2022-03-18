ebb9c501d4cd8eea5f0e80da3bbefece2bd431c1
Pete Hall <pete.mack.hall@gmail.com>
2022-03-18 18:02:44 -0500

    first pass at loops practice

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,18 +13,29 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for (let i = 3; i < 6; i++) {
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for (let i = 2; i < 11; i+=2) {
+  console.log(i);
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i = 5; i > -1; i--) {
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -32,16 +43,34 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for (star of stars) {
+  console.log(star);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let index = 0;
+while (index < stars.length) {
+  console.log(stars[index]); // I was struggling logging each star individually (I could get it to log the array as a whole). This website helped me understand that I could use my index variable, which is a number, to log the specific index of the stars array with each loop (and the index will increase each time, giving me the next star, becase I'm incrementing the index variable each loop) https://levelup.gitconnected.com/five-ways-to-loop-through-a-javascript-array-3325f4673334
+  index++;
+  //console.log(index);
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+let numberCount = 0;
+while (numberCount < 6) {
+  console.log(numberCount);
+  numberCount++
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let numberBackwards = 10; // so a while statement always needs to reference a variable that can change? (so it's not an infinite loop?)
+while (numberBackwards > 4) {
+  console.log(numberBackwards);
+  numberBackwards--;
+}
